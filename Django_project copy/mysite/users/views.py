f5bc5bc895e809f65d1450361820c81698ec3b5a
nsh2207 <sn4165@nyu.edu>
2024-10-14 22:29:13 -0400

    Working on user registration feature

---
+++ Django_project copy/mysite/users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            return redirect('home')  # Redirect to home page or wherever you want
+    else:
+        form = SignUpForm()
+    return render(request, 'users/signup.html', {'form': form})
