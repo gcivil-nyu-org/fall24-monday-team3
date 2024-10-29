91a054cf847a85301c50bfdeb482d99befd63504
nsh2207 <sn4165@nyu.edu>
2024-10-28 20:37:34 -0400

    displace travis.yml file

--- listings/forms.py
+++ listings/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Rental
 
+
 class RentalForm(forms.ModelForm):
     class Meta:
         model = Rental
-        fields = ['title', 'description', 'price', 'image', 'location', 'zip_code', 'apartment_type', 'sqft', 'amenities']
+        fields = [
+            "title",
+            "description",
+            "price",
+            "image",
+            "location",
+            "zip_code",
+            "apartment_type",
+            "sqft",
+            "amenities",
+        ]
