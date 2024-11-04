d8cf45761c99ada794aa11c23ae60f34037ed4cc
nsh2207 <sn4165@nyu.edu>
2024-11-04 13:05:08 -0500

    Deploying test branch through travis and deleted sample.py

--- rentals/sample.py
+++
@@ -1,3 +0,0 @@
-from anomalib.models.image.patchcore.torch_model import PatchcoreModel
-layers = ["layer2", "layer3"]
-model = PatchcoreModel(layers, backbone='wide_resnet50_2', pre_trained=True, num_neighbors=9)
