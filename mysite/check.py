145143256f09b7176b52d5841044afe5272524f5
nsh2207 <sn4165@nyu.edu>
2024-11-05 17:01:18 -0500

    Trying CI/CD using the test DB on AWS

--- mysite/check.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv("database.env")
-print("Database Name:", os.getenv('DATABASE_NAME'))
-print("Database User:", os.getenv('DATABASE_USER'))
-print("Database Host:", os.getenv('DATABASE_HOST'))
-print("Database Port:", os.getenv('DATABASE_PORT'))
