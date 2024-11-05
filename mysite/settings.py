34a1e667861fdf957e1cd8ae52eafc7532e9f76c
nsh2207 <sn4165@nyu.edu>
2024-11-05 16:17:47 -0500

    Trying CI/CD using the test DB on AWS

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),
+        'OPTIONS': {
+            'options': '-c timezone=utc',
+        },
     }
 }
 
