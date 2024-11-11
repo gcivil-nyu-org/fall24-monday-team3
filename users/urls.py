a911528c5757d04f69bbe40cbfc08f79fddc2bf5
ivarfan <nafhcaz@gmail.com>
2024-11-11 16:38:34 -0500

    formatting

--- users/urls.py
+++ users/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", login_view, name="login"),
     path("register/", register_view, name="register"),
-    path(
-        "logout/", LogoutView.as_view(next_page="home"), name="logout"
-    ),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
     path("profile/", views.profile_view, name="profile"),
 ]
