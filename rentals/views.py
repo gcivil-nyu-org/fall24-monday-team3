3fbeb1ac1478f89dd410197a248b58385ad19388
nsh2207 <sn4165@nyu.edu>
2024-11-10 22:02:34 -0500

    Finalizing my changes

--- rentals/views.py
+++ rentals/views.py
@@ -8,7 +8,7 @@ from django.db.models import Avg, Q
 import requests
 import os
 from dotenv import load_dotenv
-load_dotenv("/Users/sreeharshnamani/Downloads/Assignments_NYU/Software/fresh_rentsense/mysite/rentals/map-api.env")
+load_dotenv("/Users/sreeharshnamani/Downloads/Assignments_NYU/Software/fresh_rentsense/mysite/rentals/map.env")
 
 # import PIL
 
