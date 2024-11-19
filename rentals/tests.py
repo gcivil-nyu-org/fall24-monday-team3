0c89da17f6110c8dc0508b0699c64c58af2f0f85
nsh2207 <sn4165@nyu.edu>
2024-11-18 22:35:59 -0500

    Incorporated user profile features

--- rentals/tests.py
+++ rentals/tests.py
@@ -26,7 +26,6 @@ class ApartmentPostOwnershipTest(TestCase):
             address="123 Test St",
             bedrooms=2,
             square_feet=1000,
-            post_type="APARTMENT",
         )
 
         # Set up the test client
@@ -51,7 +50,6 @@ class ApartmentPostOwnershipTest(TestCase):
             "address": "123 Test St",
             "bedrooms": 2,
             "square_feet": 1000,
-            "post_type": "APARTMENT",
         }
         response = self.client.post(
             reverse("update_apartment_post", kwargs={"pk": self.apartment.pk}),
