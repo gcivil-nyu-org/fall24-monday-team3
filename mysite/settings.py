a2c0b465db8869d1957b434f3a2db626c30ef86b
nsh2207 <sn4165@nyu.edu>
2024-11-30 20:31:49 -0500

    Added messaging feature

--- mysite/settings.py
+++ mysite/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv("env")
+load_dotenv("/Users/sreeharshnamani/Downloads/Assignments_NYU/Software/fresh_rentsense/mysite/mysite/database.env")
 print("Database Name:", os.getenv("DATABASE_NAME"))
 print("Database User:", os.getenv("DATABASE_USER"))
 print("Database Host:", os.getenv("DATABASE_HOST"))
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "discussions",
     "chatbot",
     "storages",
+    "dm",
 ]
 
 MIDDLEWARE = [
