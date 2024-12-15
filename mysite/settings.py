def2f0343a542696caedc48b2fc1f466d4d77f73
ivarfan <nafhcaz@gmail.com>
2024-12-15 13:08:17 -0500

    Admin update

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,6 +85,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "mysite.wsgi.application"
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
